5b5af9f444aa454a540ed8ff70ad0617f585c913
CFL404Berisso <cfp404berisso@abc.gob.ar>
2023-12-06 19:52:43 -0300

    popup 1

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopupTMA').addEventListener('click', function() {
+    document.getElementById('popupContainerTMA').style.display = 'flex';
+});
+
+document.getElementById('closePopupTMA').addEventListener('click', function() {
+    document.getElementById('popupContainerTMA').style.display = 'none';
+});
