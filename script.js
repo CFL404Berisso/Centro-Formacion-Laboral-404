52ffa69d0411646effd641dd8fcf529281ab9b1e
Nicolas Daniel Rodriguez <Nicrod@Muni.Bsso>
2023-12-11 12:49:04 -0300

    creacion pagina mecanica

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 const courses = {
     curso1: {
-        title: "Curso 1",
-        description: "Descripción del Curso 1.",
-        media: "https://ejemplo.com/imagen_curso1.jpg", // Puedes usar una URL de imagen o un enlace a un video de YouTube, por ejemplo.
-        data: "Datos adicionales para el Curso 1.",
-        link: "https://enlace-curso1.com"
+       media: "mecanica.html",
     },
     curso2: {
-        title: "Curso 2",
-        description: "Descripción del Curso 2.",
         media: "https://ejemplo.com/video_curso2.mp4",
-        data: "Datos adicionales para el Curso 2.",
-        link: "https://enlace-curso2.com"
     },
     curso3: {
-        title: "Curso 3",
-        description: "Descripción del Curso 3.",
         media: "https://ejemplo.com/3.mp4",
-        data: "Datos adicionales para el Curso 3.",
-        link: "https://enlace-3.com"
     }
 };
 
-
 const popupContainer = document.getElementById('popupContainer');
-const courseTitle = document.getElementById('courseTitle');
-const courseDescription = document.getElementById('courseDescription');
 const courseMedia = document.getElementById('courseMedia');
-const courseData = document.getElementById('courseData');
-const courseLink = document.getElementById('courseLink');
 const openPopupButtons = document.querySelectorAll('.openPopup');
 const closePopupButton = document.getElementById('closePopup');
 
@@ -52,17 +35,7 @@ window.addEventListener('click', function(event) {
 });
 
 function showPopup(course) {
-    courseTitle.textContent = course.title;
-    courseDescription.textContent = course.description;
-
-    if (course.media.endsWith('.mp4')) {
-        courseMedia.innerHTML = `<video width="100%" height="auto" controls><source src="${course.media}" type="video/mp4"></video>`;
-    } else {
-        courseMedia.innerHTML = `<img src="${course.media}" alt="Curso Media">`;
-    }
-
-    courseData.textContent = course.data;
-    courseLink.href = course.link;
+    courseMedia.innerHTML = `<iframe src="${course.media}" width="100%" height="300" frameborder="0"></iframe>`;
     popupContainer.style.display = 'flex';
 }
 
